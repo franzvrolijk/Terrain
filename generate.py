5be37e96925621936acf1ad678432b88a3c19395
franzav <franzav@stud.ntnu.no>
2019-04-29 12:00:42 +0200

    update

--- generate.py
+++ generate.py
@@ -10,16 +10,16 @@ def genHeightMap(xp=10, yp=10, passes=1):
         arr: generated map (numpy ndarray)
     '''
     arr = np.random.rand(xp, yp)
-    for i in range(1,passes+1):
-        for y in range(1,yp-1, 1):
-            for x in range(1, xp-1, 1):
-                arr[x,y] = 0.25 * np.sum([arr[x-1, y], arr[x, y-1], arr[x, y+1], arr[x+1, y]])
+    for i in range(0, passes):
+        for y in range(1, yp - 1, 1):
+            for x in range(1, xp - 1, 1):
+                arr[x,y] = 0.25 * np.sum([arr[x - 1, y], arr[x, y - 1], arr[x, y + 1], arr[x + 1, y]])
     return arr
 
 def remEdges(arr):
     for y in range(0, arr.shape[1]):
         for x in range(0, arr.shape[0]):
-            if (x == 0 or y == 0 or x == arr.shape[0] or x == arr.shape[1]):
+            if (x == 0 or y == 0 or x == arr.shape[0] - 1 or y == arr.shape[1] - 1):
                 arr[x,y] = 0
             
-    return arr
+    return 0
